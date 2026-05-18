0b51642bd3c7e6b9c78f6aaae66d4876e6edc085
Anna Matiashchuk <anna.matjascuk@gmail.com>
2026-05-18 17:19:35 +0300

    feat: add second screen; look update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ua.university.ornamenteditor {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens ua.university.ornamenteditor to javafx.fxml;
--- src/main/java/ua/university/ornamenteditor/HelloController.java
+++ src/main/java/ua/university/ornamenteditor/HelloController.java
@@ -1,14 +1,32 @@
 package ua.university.ornamenteditor;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 //    @FXML
 //    private Label welcomeText;
 
     @FXML
-    protected void onStartButtonClick() {
+    protected void onStartButtonClick(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-screen.fxml"));
+            Scene newScene = new Scene(fxmlLoader.load());
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(newScene);
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Помилка завантаження екрана: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
     @FXML
     protected void onExitButtonClick() {
